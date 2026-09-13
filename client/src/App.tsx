a5650340bbfa463b24d3f83cd59404cdf8322ec2
mrhkw <315022068+mrhkw@users.noreply.github.com>
2026-09-13 18:13:41 +0500

    Gate UGC creation behind Terms and Community Guidelines acceptance

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AdConsentBanner } from "./components/AdConsentBanner";
 import { GlobalAdPlacement } from "./components/GlobalAdPlacement";
+import { UgcTermsGate } from "./components/UgcTermsGate";
 
 const Home = lazy(() => import("@/pages/Home"));
 const NotFound = lazy(() => import("@/pages/NotFound"));
@@ -64,5 +65,5 @@ function Router() {
 }
 
 export default function App() {
-  return <ErrorBoundary><ThemeProvider defaultTheme="light" switchable><TooltipProvider><DeferredUtilities /><GlobalAdPlacement /><Router /><AdConsentBanner /></TooltipProvider></ThemeProvider></ErrorBoundary>;
+  return <ErrorBoundary><ThemeProvider defaultTheme="light" switchable><TooltipProvider><DeferredUtilities /><GlobalAdPlacement /><Router /><AdConsentBanner /><UgcTermsGate /></TooltipProvider></ThemeProvider></ErrorBoundary>;
 }
